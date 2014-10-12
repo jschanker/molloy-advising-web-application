ea817ccdb26a77801f714f73497e2bb66217bf99
jschanker <jschanker@molloy.edu>
2014-10-11 22:27:51 -0400

    Create genes-requirements.js

---
+++ genes-requirements.js
@@ -0,0 +1,25 @@
+function getGenEdCodes()
+{
+	var artHistory = ["ART  129", "ART  220", "ART  221H", "ART  223", "ART  224", "ART  225",
+	                  "ART  228", "ART  230", "ART  232", "ART  233", "ART  236", "ART  237",
+	                  "ART  238", "ART  239", "ART  240", "ART  241", "ART  248", "ART  270",
+	                  "ART  275", "ART  296", "ART  321H", "ART  323H", "ART  324H", "ART  397", "ART  471"];
+	var musicHistory = ["MUS  105", "MUS  107", "MUS  108", "MUS  109", "MUS  164", "MUS  201", "MUS  202",
+	                  "MUS  203", "MUS  205", "MUS  208", "MUS  209", "MUS  224H", "MUS  322H", "MUS 323H",
+	                  "MUS  324H", "MUS  390", "MUS  471"]; //108 for Ed Majors only, 200 for Music Majors only
+	var communications = ["COM  110", "COM  114", "COM  117", "COM  120", "COM  122", "COM  125", "COM  210",
+	                      "COM  211", "COM  213", "COM  214", "COM  220", "COM  221", "COM  229", "COM  246",
+	                      "COM  313", "COM  314", "COM  356"]; // 229 for Business only, 124 discontinued'
+	var english = ["ENG  110"];
+	var modernLanguages = ["ARA  101", "ARA  102", "ARA  201", "CHI  101", "CHI  102", "CHI  201", "ESL  125",
+	                       "ESL  135", "ESL  136", "ESL  140", "ESL  145", "ESL  155", "FRE  101", "FRE  102",
+	                       "FRE  201", "FRE  215", "ITA  101", "ITA  102", "ITA  113", "ITA  114", "ITA  201",
+	                       "ITA  215", "ITA  305", "ITA  330", "ITA  331", "SPA  101", "SPA  102", "SPA  103",
+	                       "SPA  104", "SPA  113", "SPA  114", "SPA  201", "SPA  215", "SPA  216", "SPA  220",
+	                       "SPA  305", "SPA  307", "SPA  309", "SPA  330", "SPA  331"];
+}
+
+function fulfillAllGenEd()
+{
+	
+}
