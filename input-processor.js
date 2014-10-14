30019e660e12e9755e6b5c03fef4962868f70dd7
jschanker <jschanker@molloy.edu>
2014-10-13 22:02:27 -0400

    Update input-processor.js
    Now gives you warning about general education requirements as well

--- input-processor.js
+++ input-processor.js
@@ -56,6 +56,23 @@ function getNumOfCredits(courseHistoryItems, courseList, criteriaFunction)
 	return credits;
 }
 
+function getCourseItems(courseHistoryItems, courseList, criteriaFunction)
+{
+	// returns an array of course items taken in courseList
+	// that meet the given criteriaFunction
+
+	var courseItems = [];
+	for(var i = 0; i < courseHistoryItems.length; i++)
+	{
+		var courseCode = courseHistoryItems[i].courseAreaCode + "  " + courseHistoryItems[i].courseNumber;
+		if( courseList.indexOf(courseCode) != -1 && criteriaFunction(courseHistoryItems[i]) )
+		{
+			courseItems.push(courseHistoryItems[i]);
+		}
+	}
+	return courseItems;
+}
+
 function generateCourseCodeList(courseItems)
 {
 	// generates list of course codes from courseItems
@@ -115,6 +132,84 @@ function getLASCreditWarning(numOfLASCredits, semestersToGraduate)
 	return warning;
 }
 
+function getNeededGenEdRequirements(courseHistoryItems)
+{
+	var genEdRequirements = getGenEdObject();
+	var numOfCategories = genEdRequirements.length;
+	var gradeList = ["A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "Transfer", "WIP"];
+	var categoriesAndCreditsNeeded = [];
+	var coursesCounted = [];
+	//var historyCourseCodeList = generateCourseCodeList(courseHistoryItems);
+
+    // For now assume cross-listed courses are counted according to their current
+    // designation (e.g., HIS 213 counted as a history requirement and not as a Political Science
+    // requirement even though it's cross-listed as POL 213)
+
+	for(var i = 0; i < numOfCategories; i++)
+	{
+		var genEdCategory = genEdRequirements[i];
+		var numOfCoursesFromCategoryNeeded = parseInt(genEdCategory.neededAreas);
+		var numOfCreditsFromCategoryNeeded = parseFloat(genEdCategory.totalCredits);
+		var neededCreditsPerArea = genEdCategory.totalCredits / genEdCategory.neededAreas;
+		// assume each area is equally represented by credits in category
+		var neededCoursesPerArea = 1; // assume only 1 requirement is needed from each area
+
+		for(var j = 0; j < genEdCategory.areas.length; j++)
+		{
+			// iterate over areas in category
+			var creditsCountedForArea = 0;
+			var coursesCountedForArea = 0;
+			var courseItems = getCourseItems(courseHistoryItems, genEdCategory.areas[j], 
+				function(courseItem)
+				{
+					return gradeList.indexOf(courseItem.grade) != -1; // passing grade in course or in progress
+				}); // courseItems stores all taken courses with passing grades (or in-progress) from area
+			for(var k = 0; k < courseItems.length; k++)
+			{
+				courseDesignation = courseItems[k].courseAreaCode + "  " + courseItems[k].courseNumber;
+				if(coursesCounted.indexOf(courseDesignation) == -1)
+				{ // course not already counted
+					var creditsCountedFromCourse = Math.min(neededCreditsPerArea, parseFloat(courseItems[k].credits));
+					if(coursesCountedForArea < neededCoursesPerArea)
+					{
+						numOfCoursesFromCategoryNeeded--;
+						coursesCountedForArea++;
+						coursesCounted.push(courseDesignation);
+					}
+					if(creditsCountedForArea < neededCreditsPerArea)
+					{
+						creditsCountedForArea += creditsCountedFromCourse;
+						numOfCreditsFromCategoryNeeded -= creditsCountedFromCourse;
+						if(coursesCounted.indexOf(courseDesignation) == -1)
+							coursesCounted.push(courseDesignation);
+					}
+				}
+			}
+		}
+		if(numOfCreditsFromCategoryNeeded > 0)
+			categoriesAndCreditsNeeded.push({name: genEdCategory.name, credits:numOfCreditsFromCategoryNeeded});
+
+	}
+
+	return categoriesAndCreditsNeeded;
+}
+
+function getGenEdWarning(courseHistoryItems)
+{
+	genEdCreditsNeeded = 0;
+	categoriesAndCreditsNeeded = getNeededGenEdRequirements(courseHistoryItems);
+	warning = "For your general education requirements, you still need the following:\n";
+	for(var i = 0; i < categoriesAndCreditsNeeded.length; i++)
+	{
+		genEdCreditsNeeded += parseFloat(categoriesAndCreditsNeeded[i].credits);
+		warning += categoriesAndCreditsNeeded[i].credits + " credit(s) of " + categoriesAndCreditsNeeded[i].name + ", \n";
+	}
+
+	warning += "for a total of " + genEdCreditsNeeded + " credit(s)."
+
+	return warning;
+}
+
 function generateAdvice(courseInput)
 {
 	var courseHistoryItems = parseCourseHistory(courseInput.courseHistory);
@@ -150,6 +245,8 @@ function generateAdvice(courseInput)
 		});
 
 	var creditWarning = getCreditWarning(earnedCreditCount+creditsInProgress, courseInput.semestersToGraduate);
+	var genEdWarning = getGenEdWarning(courseHistoryItems);
+	adviceItems.push(genEdWarning);
 	adviceItems.push(creditWarning);
 	adviceItems.push(getLASCreditWarning(LASCreditCount + LASCreditsInProgress, courseInput.semestersToGraduate));
 
