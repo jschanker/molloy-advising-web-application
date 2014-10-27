d9f78d64b44ca5c4d72d029ef43b4a304c3309f9
jschanker <jschanker@molloy.edu>
2014-10-26 22:11:00 -0400

    Update math-advice.js
    Spaces between codes

--- math-advice.js
+++ math-advice.js
@@ -6,12 +6,12 @@ function getMATMajorRequirements()
 	var relatedCodes = [["CSC  103", "CSC  120", "CSC  235"], 
 	                    ["PHY  270", "PHY  271", "BIO  110", "ESC  115", "BIO  112", "BIO  120", "BIO  121", "BIO  126", "BIO  127", 
 	                     "BIO  150", "BIO  151", "BIO  280H", "CHE  103", "CHE  112", "CHE  132", "CHE  133", 
-	                     "ESC  125", "ESC  127", "ESC  130", "PHY  271", "BIOGEDL", "CHEGEDL", "ENVGEDL", "ESCGEDL", 
-	                     "PHYGEDL", "SCIMET1", "SCIMET2"],
+	                     "ESC  125", "ESC  127", "ESC  130", "PHY  271", "BIO  GEDL", "CHE  GEDL", "ENV  GEDL", "ESC  GEDL", 
+	                     "PHY  GEDL", "SCIMET1", "SCIMET2"],
 	                    ["PHY  270", "PHY  271", "BIO  110", "ESC  115", "BIO  112", "BIO  120", "BIO  121", "BIO  126", "BIO  127", 
 	                     "BIO  150", "BIO  151", "BIO  280H", "CHE  103", "CHE  112", "CHE  132", "CHE  133", 
-	                     "ESC  125", "ESC  127", "ESC  130", "PHY  271", "BIOGEDL", "CHEGEDL", "ENVGEDL", "ESCGEDL", 
-	                     "PHYGEDL", "SCIMET1", "SCIMET2"]];
+	                     "ESC  125", "ESC  127", "ESC  130", "PHY  271", "BIO  GEDL", "CHE  GEDL", "ENV  GEDL", "ESC  GEDL", 
+	                     "PHY  GEDL", "SCIMET1", "SCIMET2"]];
 
 	return {majorName: majorName,
 		    majorCodeNumbers: majorCodeNumbers,
