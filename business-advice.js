280d5501c3ed10c6a99bf74f7ea515ff5785c543
jschanker <jschanker@molloy.edu>
2014-10-28 23:57:00 -0400

    Update business-advice.js
    Corrected copy/paste - Changed area from MAT to BUS

--- business-advice.js
+++ business-advice.js
@@ -1,6 +1,6 @@
 function getBUSMgmtMajorRequirements()
 {
-	var majorName = "MAT";
+	var majorName = "BUS";
 	var majorCodeNumbers = [[101],[240],[260],[261],[301],[303],[304],[307],[330],[334],
 	                  [343],[350],[900],[900],[389,460,484],[490]];
 	var relatedCodes = [["CIS  105"], ["CIS  360"], ["COM  229"], ["COM  239"], ["ECO  251"], ["ECO  252"], ["ECO  315"], 
